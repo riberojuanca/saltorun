7f3f583dc1ed0d9d78e47c400d916026e911cc4a
Juanca <riberojuanca@gmail.com>
2024-03-30 17:59:47 -0300

    Finished Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import Link from "next/link";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
 
 function Footer() {
   return (
-    <section className="h-fit flex flex-col items-center justify-center p-4">
-      <article className="grid grid-cols-2">
-        <Link href="">
-          <p>Inscribirse</p>
+    <section className=" text-center text-zinc-500 h-fit flex flex-col items-center justify-center my-4">
+      <article className="text-base flex items-center justify-center gap-4">
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeUJR14q0nf-XY6hPt6LjMKGztsmWYwF1FakTSuKh_3WmuUJw/viewform?pli=1"
+          target="_blank"
+          rel="noopener"
+        >
+          Inscribirse
         </Link>
-        <Link href="">
-          <p>Reglamento</p>
+        <Link href="/reglamento">Reglamento</Link>
+        <Link
+          href="https://www.instagram.com/saltorun/"
+          target="_blank"
+          rel="noopener"
+        >
+          Instagram
         </Link>
-        <Link href="">
-          <p>Instagram</p>
-        </Link>
-        <Link href="">
-          <p>Facebook</p>
+        <Link
+          href="https://www.facebook.com/campeonatosaltorun"
+          target="_blank"
+          rel="noopener"
+        >
+          Facebook
         </Link>
       </article>
-      <Link href={"https://luzbelito.com"}>🕯️Luzbelito</Link>
+      {/* <hr className="w-80 my-6 opacity-5" /> */}
+      <div className=" w-full ms-auto me-auto mt-4 mb-6 ">
+        <small>SALTO RUN © POR </small>
+        <Link
+          className="mr-auto grayscale hover:grayscale-0"
+          href={"https://luzbelito.com"}
+        >
+          🕯️<small>LUZBELITO</small>
+        </Link>
+      </div>
     </section>
   );
 }
