05decf1ac168eafd542e2010ba0657b90099d4cf
Juanca <riberojuanca@gmail.com>
2024-04-04 20:41:13 -0300

    new test Api revalidate

--- src/app/api/revalidate/route.jsx
+++ src/app/api/revalidate/route.jsx
@@ -1,6 +1,7 @@
+import GeneralMujeres from "@/app/tables/mujeres/GeneralMujeres/page";
 import { revalidateTag } from "next/cache";
 
 export async function GET() {
-  revalidateTag("csv");
+  revalidateTag(GeneralMujeres);
   return Response.json({ revalidated: true });
 }
