10055a7f37ed71d5958191abd7c2db5c6f185345
Juanca <riberojuanca@gmail.com>
2024-04-05 00:51:59 -0300

    Championes images was added

--- src/app/tables/hombres/page.jsx
+++ src/app/tables/hombres/page.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
 function page() {
   return (
     <main className=" w-full ms-auto me-auto p-6">
-      {/* <div>AQUI ENCONTRARAS TODAS LAS TABLAS GENERALES DE HOMBRES</div>
-      <p>CREAR FOOTER AQUI ABAJO Y ELIMINAR ESTO CUANDO ESO PASE</p> */}
+      <div className=" max-w-2xl relative ms-auto me-auto">
+        <Image
+          className=" rounded-md ms-auto me-auto"
+          src={"/bryan.jpg"}
+          alt="Foto de Bryan Ribero, ganador edición 2023"
+          width={676}
+          height={676}
+        ></Image>
+        <div className=" bgBryan text-white top-8 left-0 p-2 rounded-e-sm absolute ">
+          Bryan Ribero, ganador edición 2023
+        </div>
+      </div>
     </main>
   );
 }
--- src/app/tables/mujeres/page.jsx
+++ src/app/tables/mujeres/page.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Image from "next/image";
+
 function page() {
   return (
     <main className=" w-full ms-auto me-auto p-6">
-      {/* <div>AQUI ENCONTRARAS TODAS LAS TABLAS GENERALES DE MUJERES</div>
-      <p>CREAR FOOTER AQUI ABAJO Y ELIMINAR ESTO CUANDO ESO PASE</p> */}
+      <div className=" max-w-2xl relative ms-auto me-auto">
+        <Image
+          className=" rounded-md ms-auto me-auto"
+          src={"/greta.jpg"}
+          alt="Foto de Greta Rodríguez, ganadora edición 2023"
+          width={676}
+          height={676}
+        ></Image>
+        <div className=" bgGreta text-white top-8 left-0 p-2 rounded-e-sm absolute ">
+          Greta Rodríguez, ganadora edición 2023
+        </div>
+      </div>
     </main>
   );
 }
